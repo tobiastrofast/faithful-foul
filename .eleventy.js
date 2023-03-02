c1dbd85e4ef370edd6e53aee4eca34fc626162af
Tobias Trofast <tobias.trofast@gmail.com>
2023-03-02 13:22:04 +0100

    Total remake of SSG to 11ty. Updated to 2.0

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+// .eleventy.js
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('./src/images/');
+    eleventyConfig.addPassthroughCopy('./src/css/');
+    eleventyConfig.setServerOptions({
+        port: 8090
+    })
+    return {
+        // markdownTemplateEngine: 'njk',
+        // dataTemplateEngine: 'njk',
+        // htmlTemplateEngine: 'njk',
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    }
+};
