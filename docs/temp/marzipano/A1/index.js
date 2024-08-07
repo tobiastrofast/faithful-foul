d75a8020c2459c352b9743f8a1ae9f97fab61732
Tobias Trofast <tobias.trofast@gmail.com>
2024-08-07 08:52:15 +0200

    Init commit to new multi-room branch. Moving common files such as images, css etc to new "common" folder and vender specifics into "vender" folder. Room specific files in "A1" folder.
    Changing the file path references in index.html and index.js files.

--- docs/temp/marzipano/A1/index.js
+++ docs/temp/marzipano/A1/index.js
@@ -253,9 +253,9 @@
 
     // Create image element.
     var icon = document.createElement('img');
-    // icon.src = 'img/link.png';
-    // icon.src = 'img/link_360.png';
-    icon.src = 'img/link_viewpoint.png';
+    // icon.src = '../common/img/link.png';
+    // icon.src = '../common/img/link_360.png';
+    icon.src = '../common/img/link_viewpoint.png';
     icon.classList.add('link-hotspot-icon');
 
     // Set rotation transform.
@@ -301,7 +301,7 @@
     var iconWrapper = document.createElement('div');
     iconWrapper.classList.add('info-hotspot-icon-wrapper');
     var icon = document.createElement('img');
-    icon.src = 'img/info.png';
+    icon.src = '../common/img/info.png';
     icon.classList.add('info-hotspot-icon');
     iconWrapper.appendChild(icon);
 
@@ -317,7 +317,7 @@
     var closeWrapper = document.createElement('div');
     closeWrapper.classList.add('info-hotspot-close-wrapper');
     var closeIcon = document.createElement('img');
-    closeIcon.src = 'img/close.png';
+    closeIcon.src = '../common/img/close.png';
     closeIcon.classList.add('info-hotspot-close-icon');
     closeWrapper.appendChild(closeIcon);
 
